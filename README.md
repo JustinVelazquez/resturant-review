f6086f5748900b1a5a133e1280feb711d3afae37
Justin V <justinv4994@gmail.com>
2021-06-28 12:31:51 -0400

    init and connect to Server/Database

---
+++ README.md
@@ -0,0 +1,10 @@
+Resturant Review APP
+
+This MERN fullstack resturant review application is a review exercise in creating a fullStack Web Application.
+
+Technolgies Used:
+
+1) React
+2) Express
+3) MongoDb
+4) Node.js
---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+const connectDb = require('../backend/config/database');
+
+dotenv.config();
+const app = express();
+
+connectDb();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
+app.use(cors());
+
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.json('hello World');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening in on PORT: ${PORT}`);
+});
